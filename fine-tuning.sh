4d8aa4869d1d747febec076ac381c37dc5156c99
AyushhS <ayush77.2000@gmail.com>
2023-12-09 02:17:01 +0530

    corrections

--- fine-tuning.sh
+++ fine-tuning.sh
@@ -1,6 +1,6 @@
 torchrun --nproc_per_node=4 --master_port=1234 finetune.py \
     --base_model garage-bAInd/Platypus2-13B \
-    --data-path dataset.csv \
+    --data-path dataset_train.csv \
     --output_dir ./llama2-platypus-13B \
     --batch_size 16 \
     --micro_batch_size 1 \
