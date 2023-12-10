ad185e8a6846c0df4c824d982a5cc63528b84ca5
AyushhS <ayush77.2000@gmail.com>
2023-12-10 15:41:29 +0530

    .

--- merge.sh
+++ merge.sh
@@ -1,4 +1,4 @@
 python merge.py \
     --base_model_name_or_path garage-bAInd/Platypus2-7B \
-    --peft_model_path ./../llama2-platypus-7B/pytorch_model.bin \
+    --peft_model_path ./../llama2-platypus-7B \
     --output_dir ./../Platypus2-7B-merged
