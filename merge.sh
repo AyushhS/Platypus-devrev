7395bb7db6bb55d006b72c5f88f58243a2d69358
AyushhS <ayush77.2000@gmail.com>
2023-12-10 14:52:31 +0530

    changed merge and fine tune config to match VM

--- merge.sh
+++ merge.sh
@@ -1,4 +1,4 @@
 python merge.py \
-    --base_model_name_or_path Open-Orca/OpenOrcaxOpenChat-Preview2-13B \
-    --peft_model_path ./platypus-lora \
-    --output_dir ./OpenOrca-Platypus2-13B
+    --base_model_name_or_path garage-bAInd/Platypus2-7B \
+    --peft_model_path ./../llama2-platypus-7B \
+    --output_dir ./../Platypus2-7B-merged
